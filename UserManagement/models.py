25cf2d24744562cb0e575e7a572f14d106a7445b
Naitri Doshi <jesaldoshi123@gmail.com>
2023-12-07 23:15:41 +0530

    Created SignUp frontend

--- UserManagement/models.py
+++ UserManagement/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+GENDER_CHOICES = (
+    ('M', 'Male'),
+    ('F', 'Female')
+)
+
+ROLE_CHOICES = (
+    ('DOCTOR', 'Doctor'),
+    ('PATIENT', 'Patient')
+)
+
+
+class User(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    gender = models.CharField(choices=GENDER_CHOICES)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    address = models.TextField(max_length=500)
+    country = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="profile")
+    role = models.CharField(choices=ROLE_CHOICES)
